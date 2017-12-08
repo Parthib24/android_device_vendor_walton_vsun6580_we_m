04876a7cdcc73edc370545119fced4aa9d71b207
Parthib24 <sirajammunirparthi24@gmail.com>
2017-12-08 13:08:11 +0600

    v3702: first commit
    Signed-off-by: Parthib24 <sirajammunirparthi24@gmail.com>

--- HT16-vendor.mk
+++ HT16-vendor.mk
@@ -1,10 +1,10 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Homtom/HT16/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/v3702/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Homtom/HT16/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/v3702/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Homtom/HT16/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/v3702/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Homtom/HT16/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/v3702/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Homtom/HT16/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/v3702/proprietary/xbin,system/xbin)
 
