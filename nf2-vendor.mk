9fa62d7c5d8499b15c16683420c39d5b24667c82
Parhib24 <sirajammunirparthi24@gmail.com>
2018-04-10 21:52:52 +0600

    vsun6580_we_m: update vendor blobs from Primo_NF2_S008_19072016 OTA
    Signed-off-by: Parhib24 <sirajammunirparthi24@gmail.com>

--- nf2-vendor.mk
+++ nf2-vendor.mk
@@ -1,14 +1,12 @@
-ifndef VK
-$(warning included.!)
-VK := I
+VND_PATH := device/vendor/walton/vsun6580_we_m
+COPY_VENDOR_BY_FOLDER :=true
+
+ifeq ($(COPY_VENDOR_BY_FOLDER),true)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VND_PATH)/xbin,system/xbin)
+else
 endif
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/proprietary/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/proprietary/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/proprietary/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/proprietary/vendor,system/vendor)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/proprietary/xbin,system/xbin)
-
